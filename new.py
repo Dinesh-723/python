95d61f641c82c24cfbcf6a59ac89bad622750f42
Dinesh-723 <mdinesh23108@gmail.com>
2024-08-30 11:51:33 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -1,22 +1,18 @@
-from flask import Flask,render_template,url_for,redirect
-
+from flask import Flask,render_template
+import random
 app=Flask(__name__)
 
-@app.route('/')
-def myfun():
-    return "hello this is flask"
-@app.route('/Home/<int:id>')
-def Home(id):
-    a=id+id
-    print(a)
-    return "the value is %s" %a
-@app.route('/temp')
-def temp():
-    return render_template('hi.html')
-@app.route('/url')
-def url():
-    return redirect(url_for('myfun'))
+words=["M.Sujatha", "M.Dinesh", "N.Swathi","N.Naveen", "N.Kishore", "SK.Kayyum", "SK.Ibbu", "SK.Rabbnai", "J.Anusha","J.Sreelatha"]
 
+@app.route('/')
+def member():
+       return render_template('hi.html')
+
+
+@app.route('/chiti', methods=['GET'])
+def members():
+        genword=random.choice(words)
+        return render_template('hi.html',name=genword)
 
 if __name__=="__main__":
-    app.run()
+       app.run()
